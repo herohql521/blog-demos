7c6112a0e3f2589c81cc7bf14138b9ec60deb290
hql <810551604@qq.com>
2020-09-30 10:03:35 +0800

    musicbug

--- TheAviator/js/game.js
+++ TheAviator/js/game.js
@@ -266,11 +266,9 @@ function handleTouchMove(event) {
     mousePos = {x:tx, y:ty};
     //声音不播放
     if(!bgmSound.isPlaying){
-      alert.log('bfbgm')
       bgmSound.play()
     }
     if(!oceanSound.isPlaying){
-      alert.log('bfoceam')
       oceanSound.play()
     }
     
